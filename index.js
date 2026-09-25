07c1189054b7b29184d6f626b47efdca6cdc2c51
ayushrawat98 <aayushrawat0@gmail.com>
2026-09-25 12:18:33 +0530

    Port update

--- index.js
+++ index.js
@@ -53,6 +53,6 @@ nunjucksEnv.addFilter('safeFileName', (name) => {
 
 app.use('', boardRoute)
 
-app.listen(3000, "::", () => {
-	console.log("Server started at port 3000")
+app.listen(3100, "::", () => {
+	console.log("Server started at port 3100")
 })
